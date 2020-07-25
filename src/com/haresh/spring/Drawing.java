ec7348a35c7b0e8b2a0bf91f53caa0b5da7ab5ec
hareshshaha7 <haresh.shaha7@gmail.com>
2020-07-25 20:51:56 +0530

    First Commit

---
+++ src/com/haresh/spring/Drawing.java
@@ -0,0 +1,33 @@
+package com.haresh.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class Drawing {
+
+	public static void main(String[] args) {
+//		Traditional way
+//		Triangle triangle = new Triangle();
+//		triangle.draw();
+		
+//		Spring way using BeanFactory
+//		@SuppressWarnings("deprecation")
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/spring.xml"));
+//		Triangle triangle = (Triangle)factory.getBean("triangle");
+//		triangle.draw();
+
+//		Spring way using BeanFactory
+		@SuppressWarnings("deprecation")
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle");
+		triangle.draw();
+		
+		Triangle triangle1 = (Triangle)context.getBean("triangle1");
+		triangle1.draw();
+
+	}
+
+}
---
+++ src/com/haresh/spring/Triangle.java
@@ -0,0 +1,26 @@
+package com.haresh.spring;
+
+public class Triangle {
+	
+	private String type;
+	
+	public Triangle() {
+	}
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println(getType() + " Triangle Drawn");
+	}
+
+}
