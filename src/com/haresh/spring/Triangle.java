291c8507caee7b8d1e6a31a19fa566d1995981aa
hareshshaha7 <haresh.shaha7@gmail.com>
2020-07-25 21:05:36 +0530

    Added point coordinates for the triangle

--- src/com/haresh/spring/Triangle.java
+++ src/com/haresh/spring/Triangle.java
@@ -3,6 +3,9 @@ package com.haresh.spring;
 public class Triangle {
 	
 	private String type;
+	private Point a;
+	private Point b;
+	private Point c;
 	
 	public Triangle() {
 	}
@@ -19,8 +22,33 @@ public class Triangle {
 		this.type = type;
 	}
 
+	
+	public Point getA() {
+		return a;
+	}
+
+	public void setA(Point a) {
+		this.a = a;
+	}
+
+	public Point getB() {
+		return b;
+	}
+
+	public void setB(Point b) {
+		this.b = b;
+	}
+
+	public Point getC() {
+		return c;
+	}
+
+	public void setC(Point c) {
+		this.c = c;
+	}
+
 	public void draw() {
-		System.out.println(getType() + " Triangle Drawn");
+		System.out.println(getType() + " Triangle Drawn, with co-ordinates "+ a +", "+ b +", "+ c);
 	}
 
 }
